f1904409e2d231321402bacf636188cf52fad51e
analhere <analshaju654@gmail.com>
2025-01-22 15:55:26 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,18 @@
+
+    
+function addnumb(a,b){
+let c = a+b;
+console.log(c);
+}
+addnumb(2, 3);
+
+function fullname(first_name,last_name){
+ let c = first_name + last_name;
+ console.log(c);
+}
+fullname("Tiya","stephen");
+
+// loops 
+for(let i=1; i <=5; i++) {
+console.log(i);    
+}
